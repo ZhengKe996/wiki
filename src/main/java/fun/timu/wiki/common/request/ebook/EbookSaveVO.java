e0b8960a2152af55543fd432f3588324a817649a
zhengke <zhengke996@163.com>
2023-07-04 13:48:45 +0800

    feat: 参数校验(CUD)

--- src/main/java/fun/timu/wiki/common/request/ebook/EbookSaveVO.java
+++ src/main/java/fun/timu/wiki/common/request/ebook/EbookSaveVO.java
@@ -1,7 +1,9 @@
 package fun.timu.wiki.common.request.ebook;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -10,48 +12,22 @@ import java.io.Serializable;
 @Setter
 public class EbookSaveVO implements Serializable {
     private Long id;
-    /**
-     * 名称
-     */
+
+    @NotNull(message = "【书名】不能为空")
     private String name;
 
-    /**
-     * 分类1
-     */
     private Long category1Id;
 
-    /**
-     * 分类2
-     */
     private Long category2Id;
 
-    /**
-     * 描述
-     */
     private String description;
 
-    /**
-     * 封面
-     */
     private String cover;
 
-    /**
-     * 文档数
-     */
     private Integer docCount;
 
-    /**
-     * 阅读数
-     */
     private Integer viewCount;
 
-    /**
-     * 点赞数
-     */
     private Integer voteCount;
-
-    @TableField(exist = false)
-    private static final long serialVersionUID = 1L;
-
 
 }
