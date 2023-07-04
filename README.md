ae42521c573de94c1318766cddc6436866a9576d
zhengke <zhengke996@163.com>
2023-07-04 17:24:04 +0800

    feat: 无限级树设计

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Wiki
+# Wiki 知识库
+
+SpringBoot3.x + Mybatis Plus3.5.x 开发的知识库项目
+
+随缘更新~ 
--- docs/all.sql
+++ docs/all.sql
@@ -24,3 +24,26 @@ insert into `ebook` (id, name, description)
 values (3, 'Python入门教程', '零基础入门Python开发');
 insert into `ebook` (id, name, description)
 values (4, 'Golang入门教程', '零基础入门Go开发');
+
+# 分类
+drop table if exists `category`;
+create table `category`
+(
+    `id`     bigint      not null comment "id",
+    `parent` bigint      not null comment "父ID",
+    `name`   varchar(50) not null comment "名称",
+    `sort`   int comment "顺序",
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment "分类";
+
+insert into `category`(id, parent, name, sort) value (100, 000, '前端开发', 100);
+insert into `category`(id, parent, name, sort) value (101, 100, 'Vue', 101);
+insert into `category`(id, parent, name, sort) value (102, 100, 'HTML & CSS', 102);
+insert into `category`(id, parent, name, sort) value (200, 000, 'Java', 200);
+insert into `category`(id, parent, name, sort) value (201, 200, 'SpringBoot', 201);
+insert into `category`(id, parent, name, sort) value (202, 200, 'Spring MVC', 202);
+
+insert into `category`(id, parent, name, sort) value (300, 000, 'Python', 300);
+insert into `category`(id, parent, name, sort) value (301, 300, 'Scrapy', 301);
+insert into `category`(id, parent, name, sort) value (302, 300, 'XPath', 302);
