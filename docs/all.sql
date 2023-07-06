6656ba3dd91fd8cf3cb1985ad3640eb3b41e625c
zhengke <zhengke996@163.com>
2023-07-06 14:00:04 +0800

    docs: 建表文件优化

--- docs/all.sql
+++ docs/all.sql
@@ -9,21 +9,13 @@ create table `ebook`
     `category2_id` bigint comment "分类2",
     `description`  varchar(200) comment "描述",
     `cover`        varchar(200) comment "封面",
-    `doc_count`    int comment "文档数",
-    `view_count`   int comment "阅读数",
-    `vote_count`   int comment "点赞数",
+    `doc_count`    int default 0 comment "文档数",
+    `view_count`   int default 0 comment "阅读数",
+    `vote_count`   int default 0 comment "点赞数",
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment ="电子书";
 
-insert into `ebook` (id, name, description)
-values (1, 'SpringBoot入门教程', '零基础入门Java开发');
-insert into `ebook` (id, name, description)
-values (2, 'Vue入门教程', '零基础入门Web开发');
-insert into `ebook` (id, name, description)
-values (3, 'Python入门教程', '零基础入门Python开发');
-insert into `ebook` (id, name, description)
-values (4, 'Golang入门教程', '零基础入门Go开发');
 
 # 分类
 drop table if exists `category`;
@@ -36,18 +28,6 @@ create table `category`
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment "分类";
-
-insert into `category`(id, parent, name, sort) value (100, 000, '前端开发', 100);
-insert into `category`(id, parent, name, sort) value (101, 100, 'Vue', 101);
-insert into `category`(id, parent, name, sort) value (102, 100, 'HTML & CSS', 102);
-insert into `category`(id, parent, name, sort) value (200, 000, 'Java', 200);
-insert into `category`(id, parent, name, sort) value (201, 200, 'SpringBoot', 201);
-insert into `category`(id, parent, name, sort) value (202, 200, 'Spring MVC', 202);
-
-insert into `category`(id, parent, name, sort) value (300, 000, 'Python', 300);
-insert into `category`(id, parent, name, sort) value (301, 300, 'Scrapy', 301);
-insert into `category`(id, parent, name, sort) value (302, 300, 'XPath', 302);
-
 
 # 文档表
 drop table if exists `doc`;
@@ -63,20 +43,6 @@ create table `doc`
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment ="文档";
-
-insert into `doc`(id, ebook_id, parent, name, sort, view_count, vote_count)
-values (1, 1, 0, '文档1', 1, 0, 0);
-insert into `doc`(id, ebook_id, parent, name, sort, view_count, vote_count)
-values (2, 1, 1, '文档1.1', 1, 0, 0);
-insert into `doc`(id, ebook_id, parent, name, sort, view_count, vote_count)
-values (3, 1, 0, '文档2', 2, 0, 0);
-insert into `doc`(id, ebook_id, parent, name, sort, view_count, vote_count)
-values (4, 1, 3, '文档2.1', 1, 0, 0);
-insert into `doc`(id, ebook_id, parent, name, sort, view_count, vote_count)
-values (5, 1, 3, '文档2.2', 2, 0, 0);
-insert into `doc`(id, ebook_id, parent, name, sort, view_count, vote_count)
-values (6, 1, 5, '文档2.2.1', 1, 0, 0);
-
 
 CREATE TABLE `content`
 (
